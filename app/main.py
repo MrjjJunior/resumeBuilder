c2df59ea2b9aec6af8e60e03b70b52124c8acaf0
Tshepiso <tlhongtshepiso2@gmail.com>
2026-06-10 22:25:03 +0200

    HTML response to fastapi main. Now I am able to render HTML templates using Jinja2Templates This allows me to also pass variables to the HTML templates so they can be presented in the front-end

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="static/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request)-> HTMLResponse:
+    return templates.TemplateResponse(request=request, name="index.html")
