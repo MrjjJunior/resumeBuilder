c60c0ce55125f65877b51085864b30b59d6283d0
Tshepiso <tlhongtshepiso2@gmail.com>
2026-06-22 20:51:41 +0200

    Learning mongo db

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,12 +1,15 @@
 import pymongo as pm
 
-client = pm.MongoClient("mongodb://localhost:3000/")
+client = pm.MongoClient("mongodb://localhost:27017/")
 
 # print(client)
 
 database = client["DataBase"]
+column  = database["user"]
 
+user_info = {"name": "Tshepsio", "email" : "tlhongtshepiso2@gmail.com"}
 
+insert = column.insert_one(user_info)
 
 print(client.list_database_names())
 
